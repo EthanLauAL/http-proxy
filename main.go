2ea785691cd067d01a74b4e6f18c82af523f4d3e
liuchang <liuchang@yimian.com.cn>
2015-05-22 16:11:19 +0800

    增加代理账号密码功能

--- main.go
+++ main.go
@@ -1,13 +1,34 @@
 package main
 
 import (
+	"fmt"
+	"flag"
 	"io"
+	"log"
 	"net/http"
 )
 
+var (
+	argListen string
+	argUser string
+	argPass string
+)
+
 func main() {
+	flag.StringVar(&argListen, "listen", "", "Proxy listen, required.")
+	flag.StringVar(&argUser, "user", "", "Proxy username")
+	flag.StringVar(&argPass, "pass", "", "Proxy password")
+	flag.Parse()
+
+	if argListen == "" {
+		log.Fatalln("-listen argument required.")
+	}
+
+	BasicAuthStr = "Basic " + base64.StdEncoding.EncodeToString(
+		[]byte(argUserPass))
+
 	http.HandleFunc("/", handleProxy)
-	http.ListenAndServe(":3128", nil)
+	log.Fatalln(http.ListenAndServe(argListen, nil))
 }
 
 func handleProxy(w http.ResponseWriter, r *http.Request) {
@@ -16,11 +37,27 @@ func handleProxy(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if r.Method == "CONNECT" {
+		r.URL.Scheme = "http"
+		r.URL.Opaque = "https"
+	}
+
+	if argUser != "" {
+		user,pass,ok := r.BasicAuth()
+		if !ok || user != argUser || pass != argPass {
+			w.WriteHeader(http.StatusProxyAuthRequired)
+			fmt.Fprintln(w, "ProxyAuthRequired")
+			return
+		}
+	}
+
 	resp,err := http.DefaultTransport.RoundTrip(r)
 	if err != nil {
-		w.WriteHeader(http.StatusGatewayTimeout)
-		w.Write([]byte(err.Error()))
+		log.Println(r.Method, r.URL, err)
+		w.WriteHeader(http.StatusBadGateway)
+		fmt.Fprintln(w, err.Error())
 	} else {
+		fmt.Println(r.Method, r.URL, resp.Status)
 		//Copy headers
 		for key,values := range resp.Header {
 			for _,value := range values {
